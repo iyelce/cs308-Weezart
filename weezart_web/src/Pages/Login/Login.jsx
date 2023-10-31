9c0f2e31333ea33e9fff91fef8901fe70130e7e5
Anil <sinanil2001@gmail.com>
2023-10-31 19:19:57 +0300

    sign up fix

--- weezart_web/src/Pages/Login/Login.jsx
+++ weezart_web/src/Pages/Login/Login.jsx
@@ -23,9 +23,7 @@ function Login() {
                             <input type="checkbox" id="remember-me" />
                             <label htmlFor="remember-me">Remember me</label>
                         </div>
-                        <div className="pass-link">
-                            <a href="#">Forgot password?</a>
-                        </div>
+                        
                     </div>
                     <div className="field">
                         <input type="submit" value="Login" />
@@ -33,6 +31,9 @@ function Login() {
                     <div className="signup-link">
                         Not a member? <a href="#">Signup now</a>
                     </div>
+                    <div className="pass-link">
+                            <a href="#">Forgot password?</a>
+                        </div>
                 </form>
             </div>
             <div className="login-logo"><img src="https://fastly.picsum.photos/id/637/200/300.jpg?hmac=_aCsxiL_H35K1JVhdKAxp9akei2mXbQ7N5N2zfAtCiE" alt="" /></div>

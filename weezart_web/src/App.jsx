f4ff296a2d93caa4208cf460c49d809a8d7489f9
Anil <sinanil2001@gmail.com>
2023-11-02 14:46:00 +0300

    login page template

--- weezart_web/src/App.jsx
+++ weezart_web/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <Login/>
-      <Signup/>
+
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>

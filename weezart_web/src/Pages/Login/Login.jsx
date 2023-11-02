27203d5bb5f78f730ddcc9850cec2f40840dff60
Anil <sinanil2001@gmail.com>
2023-11-02 15:05:58 +0300

    W-025:login page

--- weezart_web/src/Pages/Login/Login.jsx
+++ weezart_web/src/Pages/Login/Login.jsx
@@ -3,44 +3,59 @@ import './style.css';
 
 function Login() {
   return (
-   <div className="wrapper">
-        <div className="title">
-        Login Form
-        </div>
-        <div className="container">
-            <div className="login-form">
-                <form action="#">
-                    <div className="field">
-                    <input type="text" required />
-                    <label>Email Address</label>
+   
+        
+            
+                <div className="wrapper">
+                    
+
+
+                    <div className="title">
+                    Login Form
                     </div>
-                    <div className="field">
-                        <input type="password" required />
-                        <label>Password</label>
-                    </div>
-                    <div className="content">
-                        <div className="checkbox">
-                            <input type="checkbox" id="remember-me" />
-                            <label htmlFor="remember-me">Remember me</label>
+                    <div className="container">
+                        <div className="login-form">
+                            <form action="#">
+                                <div className="field">
+                                <input type="text" required />
+                                <label>Email Address</label>
+                                </div>
+                                <div className="field">
+                                    <input type="password" required />
+                                    <label>Password</label>
+                                </div>
+                                
+                               
+                                    
+
+                                <div className="container-1">
+                                    <div class="btn"><a href="#">Login</a></div>
+                                </div>
+                                <div className="pass-link">
+                                        <a href="#">Forgot password?</a>
+                                </div>
+                                <div className="signup-link">
+                                    Not a member? <a href="#">Signup </a>
+                                </div>
+                                <div className="sign-up-socials">
+                                    <button type="button" class="login-with-google-btn block glow" >
+                                    Sign in with Google
+                                    </button>
+                                </div>
+                                
+                                
+                            </form>
                         </div>
-                        
+                        <div className="login-logo"><img src="weezart_web/src/Pages/Login/weezart-removebg-preview.png" alt="" /></div>
+
                     </div>
-                    <div className="field">
-                        <input type="submit" value="Login" />
-                    </div>
-                    <div className="signup-link">
-                        Not a member? <a >Signup now</a>
-                    </div>
-                    <div className="pass-link">
-                            <a >Forgot password?</a>
-                        </div>
-                </form>
+                
+                
             </div>
-            <div className="login-logo"><img src="https://fastly.picsum.photos/id/637/200/300.jpg?hmac=_aCsxiL_H35K1JVhdKAxp9akei2mXbQ7N5N2zfAtCiE" alt="" /></div>
-        </div>
-    
-    
-    </div>
+
+
+            
+
     
   );
 }

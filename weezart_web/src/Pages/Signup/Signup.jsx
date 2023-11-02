06bf202a64a36bddfdaa2f0ad0d44801569e0b8c
cansnnnn <narsahin161cnsn@gmail.com>
2023-11-02 13:26:13 +0300

    W0-5 signup page

--- weezart_web/src/Pages/Signup/Signup.jsx
+++ weezart_web/src/Pages/Signup/Signup.jsx
@@ -11,53 +11,91 @@ const SignUp = () => {
   const [isVisibleRepeatPassword, setIsVisibleRepeatPassword] = useState(false);
   const [password, setPassword] = useState('');
   const [repeatPassword, setRepeatPassword] = useState('');
-
-    return (
-      <div className="main">
-        <section className="signup">
-          <div className="signup-container">
-              <div className="title">Sign up</div>
-
-              <div className="signup-content">
-                <div className="signup-form">
-
-                  <form className="register-form">
-
-                    <div className="field">
-                      <input className="signup-input-box" type="text" required />
-                      <label  >Username</label>
-                    </div>
-
-                    <div className="field">
-                      <input className="signup-input-box" type="text" required />
-                      <label className="signup-input-label" >Email Address</label>
-                    </div>
-
-                    <div className="password-input-box">
-                      <div className="field">
-                        <input
-                          className="signup-input-box"
-                          type={isVisiblePassword ? 'text' : 'password'}
-                          value={password}
-                          onChange={(e) => setPassword(e.target.value)}
-                          required
-                        />
-                        <label className="signup-input-label">Password</label>
-                      </div>
-
-                      <button className="eye-button" onClick={()=> setIsVisiblePassword(!isVisiblePassword)}>
-                        {isVisiblePassword ? (
-                          <AiOutlineEye size='32px' />
-                        ) : (
-                          <AiOutlineEyeInvisible size='32px' />
-                        )}
-                      </button>
-                    </div>
+  const [username, setUsername] = useState('');
+  const [email,setEmail] = useState('');
 
 
-            <div className="password-input-box">
+  function isEmailValid(email) {
+    const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+    return emailRegex.test(email);
+  }
+
+  function registerCheck(){
+
+    console.log("--------render--------");
+
+    //check mail format
+    if (isEmailValid(email)) {
+      console.log("Email is valid");
+    } 
+    else {
+      console.log("Email is not valid");
+    }
+
+    //check password and repeat password
+    if (password === repeatPassword) {
+      console.log("Passwords are same");;
+    }
+    else {
+      console.log("Passwords do not match")
+    }
+
+    //check if the username is unique via API
+
+  }
+
+  return (
+    <div className="main">
+
+      <section className="signup">
+
+        <div className="signup-container">
+
+          <div className="title">Sign up</div>
+
+          <div className="signup-content">
+
+            <div className="signup-form">
+
+              <form className="register-form">
+
+                <div className="field">
+                  <input className="signup-input-box" type="text"  
+                    value={username}
+                    onChange={(e)=> setUsername(e.target.value)}
+                    required
+                  />
+                  <label  >Username</label>
+                </div>
+
+                <div className="field">
+                  <input className="signup-input-box" type="text" 
+                    value={email}
+                    onChange={(e)=> setEmail(e.target.value)}
+                    required
+                  />
+                  <label >Email Address</label>
+                </div>
+
+                <div className="password-input-box">
                   <div className="field">
-                    <div style={{ position: 'relative' }}>
+                    <input
+                      className="signup-input-box"
+                      type={isVisiblePassword ? 'text' : 'password'}
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                      required
+                    />
+                    <span className="eye-icon-container" onClick={()=> setIsVisiblePassword(!isVisiblePassword)}>
+                      {isVisiblePassword ? (<AiOutlineEye size="32px" />) : (<AiOutlineEyeInvisible size="32px" />)}
+                    </span>
+                    <label >Password</label>
+                  </div>
+                </div>
+            
+
+                <div className="password-input-box">
+                  <div className="field">
                       <input
                         className="signup-input-box"
                         type={isVisibleRepeatPassword ? 'text' : 'password'}
@@ -65,55 +103,45 @@ const SignUp = () => {
                         onChange={(e) => setRepeatPassword(e.target.value)}
                         required
                       />
-                      <span
-                        style={{
-                          position: 'absolute',
-                          right: '8px',
-                          top: '50%',
-                          transform: 'translateY(-50%)',
-                        }}
-                      >
-                        <button className="eye-button" onClick={()=> setIsVisibleRepeatPassword(!isVisibleRepeatPassword)}>
-                          {isVisibleRepeatPassword ? (
-                            <AiOutlineEyeInvisible size='32px' />
-                          ) : (
-                            <AiOutlineEye size='32px' />
-                          )}
-                        </button>
+                      <span className="eye-icon-container" onClick={()=> setIsVisibleRepeatPassword(!isVisibleRepeatPassword)}>
+                        {isVisibleRepeatPassword ? (
+                          <AiOutlineEye size="32px" />
+                        ) : (
+                          <AiOutlineEyeInvisible size="32px" />
+                        )}
                       </span>
-                    </div>
-                    <label className="signup-input-label">Repeat your password</label>
+                    <label >Repeat your password</label>
                   </div>
                 </div>
 
-                    <div className="field">
-                      <button className="register-button">
-                        Register
-                      </button>
-                    </div>          
+                <div className="field">
+                  <button className="register-button"
+                    onClick={registerCheck()}
+                  > Register </button>
+                </div>          
 
-                  </form>
+              </form>
 
-                  <div className="social-login">
-                    <hr className="horizontal-line" />
-                    <button type="button" class="google-sign-button" onClick={() => alert('Button clicked!')} >
-                          Sign in with Google
-                    </button>
-                  </div>
-
-                </div>
-
-                <div className="signup-image">
-                  <figure>
-                    <img src='https://i.pinimg.com/736x/7b/cf/3f/7bcf3f3ddebf6526e491a142b7e1ddad.jpg' alt="signup" />
-                  </figure>
-                </div>
+              <div className="social-login">
+                <hr className="horizontal-line" />
+                <button type="button" class="google-sign-button" onClick={() => alert('Button clicked!')} >
+                  Sign in with Google
+                </button>
               </div>
-          </div>
-        </section>
-      </div>
 
-    );
-  };
+            </div>
+
+            <div className="signup-image">
+              <figure>
+                <img src='https://i.pinimg.com/736x/7b/cf/3f/7bcf3f3ddebf6526e491a142b7e1ddad.jpg' alt="signup" />
+              </figure>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+
+  );
+};
   
   export default SignUp;

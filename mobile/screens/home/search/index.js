cdd172daed60eb89fa885bf026b0e81f2f601059
Raman Afravi <raman@afravi.com>
2023-11-02 11:08:59 +0300

    fixed main page issue

---
+++ mobile/screens/home/search/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+export default Search = () => {
+  return (
+    <SafeAreaView style={{backgroundColor: 'white'}}>
+      <View
+        style={{
+          height: '100%',
+        }}></View>
+    </SafeAreaView>
+  );
+};

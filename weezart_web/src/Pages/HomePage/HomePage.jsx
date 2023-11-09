e96d88c8ca05e382bbdbf2e020ceff972d14c0da
Anil <sinanil2001@gmail.com>
2023-11-09 17:20:04 +0300

    home page design start

---
+++ weezart_web/src/Pages/HomePage/HomePage.jsx
@@ -0,0 +1,50 @@
+import React, { useEffect } from 'react';
+import { AiOutlineEye, AiOutlineEyeInvisible } from 'react-icons/ai';
+import { useState } from "react";
+import './HomePage.css';
+
+
+
+const HomePage = () => {
+  
+
+
+
+    
+  
+    
+
+  return (
+    
+    <div className="whole-page">
+        <div className="sidebar">
+            <div className="profile">
+                profile
+            </div>
+            <div className="home">
+                home
+            </div>
+            <div className="analyse">
+                analyse
+            </div>
+            <div className="add-song">
+                add-song
+            </div>
+            <div className="logo">
+                logo
+
+            </div>
+        </div>
+    <div className="main-page">main page</div>
+    <div className="friend-activity">friends</div>
+
+
+
+
+</div>
+
+  );
+}
+
+
+export default HomePage;

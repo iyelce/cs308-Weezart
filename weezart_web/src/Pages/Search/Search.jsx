0da70e473933aff34e9bdba49392de246ced5795
cansnnnn <narsahin161cnsn@gmail.com>
2023-11-16 14:56:42 +0300

    W0-53 router update

--- weezart_web/src/Pages/Search/Search.jsx
+++ weezart_web/src/Pages/Search/Search.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import SongInfoPopup from "../Popups/SongInfoPopup";
 import ArtistInfoPopup from "../Popups/ArtistInfoPopup";
 import AlbumInfoPopup from "../Popups/AlbumInfoPopup";
+import './Search.css'
 
 
 function Search() {
@@ -105,52 +106,213 @@ function Search() {
     setShowAlbumPopups(newShowAlbumPopups);
   };
 
+// --------------------------------
 
-  
+  const [activeButton, setActiveButton] = useState(null);
+  const [activeCategory, setActiveCategory] = useState(null);
+
+  const handleButtonClick = (buttonName) => {
+    setActiveButton(buttonName);
+    // Set active category based on the clicked button
+    switch (buttonName) {
+      case 'songs':
+        setActiveCategory('songs');
+        break;
+      case 'artists':
+        setActiveCategory('artists');
+        break;
+      case 'albums':
+        setActiveCategory('albums');
+        break;
+      case 'profiles':
+        setActiveCategory('profiles');
+        break;
+      default:
+        setActiveCategory(null);
+        break;
+    }
+  };
+
+
+  const renderSearchItems = () => {
+    switch (activeCategory) {
+      case 'songs':
+        return songInfos.map((songInfo, index) => (
+          <div key={index}>
+            <button onClick={() => handleSongButtonClick(index)}>Song {index + 1}</button>
+            <SongInfoPopup
+              isOpen={showSongPopups.get(index) || false}
+              onRequestClose={() => handleSongClosePopup(index)}
+              songInfo={songInfo}
+              showModal={showSongPopups}
+            />
+            <br />
+            <br />
+          </div>
+        ));
+
+      case 'artists':
+        return artistInfos.map((artistInfo, index) => (
+          <div key={index}>
+            <button onClick={() => handleArtistButtonClick(index)}>Artist {index + 1}</button>
+            <ArtistInfoPopup
+              isOpen={showArtistPopups.get(index) || false}
+              onRequestClose={() => handleArtistClosePopup(index)}
+              artistInfo={artistInfo}
+              showModal={showSongPopups}
+            />
+            <br />
+            <br />
+          </div>
+        ));
+
+      case 'albums':
+        return albumInfos.map((albumInfo, index) => (
+          <div key={index}>
+            <button onClick={() => handleAlbumButtonClick(index)}>Album {index + 1}</button>
+            <AlbumInfoPopup
+              isOpen={showAlbumPopups.get(index) || false}
+              onRequestClose={() => handleAlbumClosePopup(index)}
+              albumInfo={albumInfo}
+              showModal={showAlbumPopups}
+            />
+            <br />
+            <br />
+          </div>
+        ));
+
+      case 'profiles':
+        return <div>Profiles</div>;
+
+      default:
+        return null;
+    }
+  };
+
+
+
+
+
+
+
+
+
 
   return (
-    <div>
-      {songInfos.map((songInfo, index) => (
-        <div key={index}>
-          <button onClick={() => handleSongButtonClick(index)}>Song {index + 1}</button>
-          <SongInfoPopup
-            isOpen={showSongPopups.get(index) || false}
-            onRequestClose={() => handleSongClosePopup(index)}
-            songInfo={songInfo}
-            showModal={showSongPopups}
-          />
-          <br />
-          <br />
-        </div>
-      ))}
+    
+     <div>
 
-    {artistInfos.map((artistInfo, index) => (
-        <div key={index}>
-          <button onClick={() => handleArtistButtonClick(index)}>Artist {index + 1}</button>
-          <ArtistInfoPopup
-            isOpen={showArtistPopups.get(index) || false}
-            onRequestClose={() => handleArtistClosePopup(index)}
-            artistInfo={artistInfo}
-            showModal={showSongPopups}
-          />
-          <br />
-          <br />
-        </div>
-      ))}
 
-    {albumInfos.map((albumInfo, index) => (
-      <div key={index}>
-        <button onClick={() => handleAlbumButtonClick(index)}>Album {index + 1}</button>
-        <AlbumInfoPopup
-          isOpen={showAlbumPopups.get(index) || false}
-          onRequestClose={() => handleAlbumClosePopup(index)}
-          albumInfo={albumInfo}
-          showModal={showAlbumPopups}/>
-        <br />
-        <br />
-        </div>))}
+      <div class="navigation-search">
+        <input type="search" placeholder="search" class="navigation-search__input" />
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="navigation-search__icon">
+          <circle cx="11" cy="11" r="8"></circle>
+          <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+        </svg>
+      </div>
+
+
+      <div className="search-buttons">
+        <button
+          className={`search-button ${activeButton === 'songs' ? 'active' : ''}`}
+          onClick={() => handleButtonClick('songs')}
+        >
+          <span>Songs</span>
+        </button>
+
+        <button
+          className={`search-button ${activeButton === 'albums' ? 'active' : ''}`}
+          onClick={() => handleButtonClick('albums')}
+        >
+          <span>Albums</span>
+        </button>
+
+        <button
+          className={`search-button ${activeButton === 'artists' ? 'active' : ''}`}
+          onClick={() => handleButtonClick('artists')}
+        >
+          <span>Artists</span>
+        </button>
+
+        <button
+          className={`search-button ${activeButton === 'profiles' ? 'active' : ''}`}
+          onClick={() => handleButtonClick('profiles')}
+        >
+          <span>Profiles</span>
+        </button>
+      </div>
+
+
+
+      <div className="search-items-container">
+        {renderSearchItems()}
+      </div>
+
+
+    <div className="search-items-container">
+    <div className="search-item">
+
+<div className="searchbar-song-number">
+  <span>
+    1
+  </span>
+</div>
+
+<img aria-hidden="false" draggable="false" loading="eager" src="https://i.scdn.co/image/ab67616d00004851aa620deeb0916090246acea1" alt="" class="mMx2LUixlnN_Fu45JpFB rkw8BWQi3miXqtlJhKg0 Yn2Ei5QZn19gria6LjZj" width="40" height="40" />
+
+<div className="searchbar-songname-artist">
+  <p className="searchbar-songname-artist-songname">Song Name</p>
+  <p className="searchbar-songname-artist-artist">artist</p>
+</div>
+
+<p className="searchbar-song-album">Album</p>
+
+<p className="seachbar-song-duration">2:30</p>
+
+</div>
 
     </div>
+      
+    
+
+    <div className="search-items-container"> 
+
+      <div className="search-item">
+        {songInfos.map((song, index) => (
+          <div key={index} className="search-item">
+            <div className="searchbar-song-number">
+              <span>{index + 1}</span>
+            </div>
+
+            <img
+              aria-hidden="false"
+              draggable="false"
+              loading="eager"
+              src={song.image || "default-image-url"} // Provide a default image URL if 'image' is undefined
+              alt={`Song ${index + 1}`}
+              className="mMx2LUixlnN_Fu45JpFB rkw8BWQi3miXqtlJhKg0 Yn2Ei5QZn19gria6LjZj"
+              width="40"
+              height="40"
+            />
+
+            <div className="searchbar-songname-artist">
+              <p className="searchbar-songname-artist-songname">{song.songName}</p>
+              <p className="searchbar-songname-artist-artist">{song.artists.join(", ")}</p>
+            </div>
+
+            <p className="searchbar-song-album">{song.album}</p>
+
+            <p className="seachbar-song-duration">{song.duration}</p>
+          </div>
+        ))}
+      </div>
+      
+    </div>
+
+
+
+    </div> 
+
   );
 }
 

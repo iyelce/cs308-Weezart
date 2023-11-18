8c91a9fad66f70fe683d68772a40325d64bf6bfa
cansnnnn <narsahin161cnsn@gmail.com>
2023-11-18 21:47:58 +0300

    asd

--- weezart_web/src/App.js
+++ weezart_web/src/App.js
@@ -7,24 +7,24 @@ import Layout from './Pages/Layout';
 import SignUp from './Pages/Authentication/SignUp';
 import Search from './Pages/Search/Search';
 import './App.css';
-import Sidebar from './Pages/Sidebar/Sidebar';
+import Sidebar from './Pages/Components/Sidebar/Sidebar.jsx'
 
 
 function App() {
   const [user, setUser] = useState(null);
   return (
-    // <BrowserRouter>
-    //   <Routes>
-    //     <Route path='/' element={<Layout />}>
-    //       <Route index element={<HomePage />} />
-    //       <Route path='login' element={<Login  ></Login>} />
-    //       <Route path='signup' element={<SignUp ></SignUp>} />
-    //       <Route path='search' element={<Search ></Search>} />
-    //       <Route path='*' element={<Error />} />
-    //     </Route>
-    //   </Routes>
-    // </BrowserRouter>
-    <Sidebar/>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path='login' element={<Login  ></Login>} />
+          <Route path='signup' element={<SignUp ></SignUp>} />
+          <Route path='search' element={<Search ></Search>} />
+          <Route path='*' element={<Error />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+    //<Sidebar/>
   );
 }
 

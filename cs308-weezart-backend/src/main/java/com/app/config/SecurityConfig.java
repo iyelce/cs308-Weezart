403d1cefe54ec4f3f3c81e6c03c0ea09206dece6
ahmetalperengungor <ahmetalperengungor@gmail.com>
2023-12-04 19:20:49 +0300

    Bugfix - bug that caused web cors error fixed

--- cs308-weezart-backend/src/main/java/com/app/config/SecurityConfig.java
+++ cs308-weezart-backend/src/main/java/com/app/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -55,7 +56,7 @@ public class SecurityConfig {
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     	log.info("!!!!!!!filter chain invoked!!!!!!!");
         http
-                .authorizeHttpRequests(authorize -> authorize
+                .cors(Customizer.withDefaults()).authorizeHttpRequests(authorize -> authorize
                                 .requestMatchers("/auth/login",
                                         "/auth/register")
                                 .permitAll()

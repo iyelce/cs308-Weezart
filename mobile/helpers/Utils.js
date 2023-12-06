6ca6341c43b6dbb9d832478080c8115d2b9f84e7
Raman Afravi <raman@afravi.com>
2023-12-06 14:07:51 +0300

    User state, saving token bugs fixed

--- mobile/helpers/Utils.js
+++ mobile/helpers/Utils.js
@@ -1,8 +1,11 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-export const saveToken = async (token) => {
+export const saveToken = async (token, userId) => {
   try {
-    await AsyncStorage.setItem("token", token);
+    await Promise.all([
+      AsyncStorage.setItem("token", token),
+      AsyncStorage.setItem("userId", userId),
+    ]);
   } catch (error) {
     console.error("Error saving token to AsyncStorage:", error);
   }
@@ -14,5 +17,13 @@ export const getToken = async () => {
     return token;
   } catch (error) {
     console.error("Error getting token from AsyncStorage:", error);
+  }
+};
+export const getUserId = async () => {
+  try {
+    const userId = await AsyncStorage.getItem("userId");
+    return userId;
+  } catch (error) {
+    console.error("Error getting userId from AsyncStorage:", error);
   }
 };

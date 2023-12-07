2271f249ce3ef1d741e6b09ed0ebfc87dfa826bc
umutkucukkayhan <umut.kucukkayhan@sabanciuniv.edu>
2023-12-07 12:52:48 +0300

    fixed a bug that allowed user to follow the same user infinite times

--- cs308-weezart-backend/src/main/java/com/app/services/AddServiceImpl.java
+++ cs308-weezart-backend/src/main/java/com/app/services/AddServiceImpl.java
@@ -4,7 +4,6 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -34,221 +33,236 @@ import com.app.repo.UserSongRepository;
 import jakarta.transaction.Transactional;
 
 @Service
-public class AddServiceImpl implements AddService{
+public class AddServiceImpl implements AddService {
 
 	@Autowired
 	private SongRepository songRepo;
-	
+
 	@Autowired
 	private UserSongRepository userSongRepo;
-	
+
 	@Autowired
 	private ArtistRepository artistRepo;
-	
+
 	@Autowired
 	private UserArtistRepository userArtistRepo;
-	
+
 	@Autowired
 	private AlbumRepository albumRepo;
-	
+
 	@Autowired
 	private UserAlbumRepository userAlbumRepo;
-	
+
 	@Autowired
 	private UserRepository userRepo;
-	
-    private static final Logger log = LoggerFactory.getLogger(SpotifyService.class);
-	
-    // TODO: spotify checkleme yapilacak
+
+	private static final Logger log = LoggerFactory.getLogger(SpotifyService.class);
+
+	// TODO: spotify checkleme yapilacak
 	public Song addSong(SongPayload song) {
-		
-		Song givenSong = new Song(song.getId(), song.getAlbumImageURL(), song.getName(), song.getAlbumName(), song.getAlbumId(), song.getArtistsName(), song.getArtistsId(), song.getPopularity(),
+
+		Song givenSong = new Song(song.getId(), song.getAlbumImageURL(), song.getName(), song.getAlbumName(),
+				song.getAlbumId(), song.getArtistsName(), song.getArtistsId(), song.getPopularity(),
 				song.getDuration_ms(), song.isExplicit(), song.getAlbumRelease());
-		
+
 		log.info("eklemeden önce var mı diye bakıyorum");
-		
-		if(songRepo.findByid(givenSong.getId()) == null){
+
+		if (songRepo.findByid(givenSong.getId()) == null) {
 			log.info("ife girdim");
 			return songRepo.save(givenSong);
 		}
 
-		else { log.info("ife girmedim"); return null; }
+		else {
+			log.info("ife girmedim");
+			return null;
+		}
 	}
-	
+
 	public UserSong relateUserSong(SongPayload song, String userID) {
-		
+
 		UserSong userSong = new UserSong();
-		
-		Song givenSong = new Song(song.getId(), song.getAlbumImageURL(), song.getName(), song.getAlbumName(), song.getAlbumId(), song.getArtistsName(), song.getArtistsId(), song.getPopularity(),
+
+		Song givenSong = new Song(song.getId(), song.getAlbumImageURL(), song.getName(), song.getAlbumName(),
+				song.getAlbumId(), song.getArtistsName(), song.getArtistsId(), song.getPopularity(),
 				song.getDuration_ms(), song.isExplicit(), song.getAlbumRelease());
-		
+
 		userSong.setSong(givenSong);
-		
+
 		User givenUser = new User(Long.parseLong(userID));
-		
+
 		userSong.setUser(givenUser);
-		
+
 		userSong.setAddTime(getCurrentDateTimeAsString());
-		
-		if(userSongRepo.findBySongAndUser(givenSong, givenUser) != null) {
+
+		if (userSongRepo.findBySongAndUser(givenSong, givenUser) != null) {
 			throw new CustomException("Song is already added");
 		}
-		
+
 		return userSongRepo.save(userSong);
-		
+
 	}
-	
+
 	public Artist addArtist(ArtistPayload artist) {
-			
-		Artist givenArtist = new Artist(artist.getName(), artist.getGenres(), artist.getImageUrl(), artist.getFollowerCount(), artist.getId());
-			
+
+		Artist givenArtist = new Artist(artist.getName(), artist.getGenres(), artist.getImageUrl(),
+				artist.getFollowerCount(), artist.getId());
+
 		log.info("eklemeden önce var mı diye bakıyorum");
-			
-		if(artistRepo.findByid(givenArtist.getId()) == null){
+
+		if (artistRepo.findByid(givenArtist.getId()) == null) {
 			log.info("ife girdim");
 			return artistRepo.save(givenArtist);
 		}
-	
-		else { log.info("ife girmedim"); return null; }
+
+		else {
+			log.info("ife girmedim");
+			return null;
+		}
 	}
-	
+
 	public UserArtist relateUserArtist(ArtistPayload artist, String userID) {
-		
+
 		UserArtist userArtist = new UserArtist();
-		
-		Artist givenArtist = new Artist(artist.getName(), artist.getGenres(), artist.getImageUrl(), artist.getFollowerCount(), artist.getId());
-	
-		
+
+		Artist givenArtist = new Artist(artist.getName(), artist.getGenres(), artist.getImageUrl(),
+				artist.getFollowerCount(), artist.getId());
+
 		userArtist.setArtist(givenArtist);
-		
+
 		User givenUser = new User(Long.parseLong(userID));
-		
+
 		userArtist.setUser(givenUser);
-		
+
 		userArtist.setAddTime(getCurrentDateTimeAsString());
-		
-		if(userArtistRepo.findByArtistAndUser(givenArtist, givenUser) != null) {
+
+		if (userArtistRepo.findByArtistAndUser(givenArtist, givenUser) != null) {
 			return userArtist;
 		}
-		
+
 		return userArtistRepo.save(userArtist);
-		
+
 	}
-	
+
 	public Album addAlbum(AlbumPayload album) {
-		
+
 		Album givenAlbum = new Album(album.getId(), album.getName(), album.getImageUrl(), album.getReleaseDate(),
-				album.getNumberOfTracks(), album.getArtistsName(), album.getArtistsId(), album.getSongsName(), album.getSongsId());
-		
-		if(albumRepo.findByid(album.getId()) == null) {
+				album.getNumberOfTracks(), album.getArtistsName(), album.getArtistsId(), album.getSongsName(),
+				album.getSongsId());
+
+		if (albumRepo.findByid(album.getId()) == null) {
 			return albumRepo.save(givenAlbum);
-		}
-		else return givenAlbum;
+		} else
+			return givenAlbum;
 	}
-	
+
 	// make a relation between user and added album
 	public UserAlbum relateUserAlbum(AlbumPayload album, String userID) {
-		
+
 		UserAlbum userAlbum = new UserAlbum();
-		
+
 		Album givenAlbum = new Album(album.getId(), album.getName(), album.getImageUrl(), album.getReleaseDate(),
-				album.getNumberOfTracks(), album.getArtistsName(), album.getArtistsId(), album.getSongsName(), album.getSongsId());
-		
+				album.getNumberOfTracks(), album.getArtistsName(), album.getArtistsId(), album.getSongsName(),
+				album.getSongsId());
+
 		userAlbum.setAlbum(givenAlbum);
-		
+
 		User givenUser = new User(Long.parseLong(userID));
-		
+
 		userAlbum.setUser(givenUser);
-		
+
 		userAlbum.setAddTime(getCurrentDateTimeAsString());
-		
-		if(userAlbumRepo.findByAlbumAndUser(givenAlbum, givenUser) != null) {
+
+		if (userAlbumRepo.findByAlbumAndUser(givenAlbum, givenUser) != null) {
 			return userAlbum;
 		}
-		
+
 		return userAlbumRepo.save(userAlbum);
 	}
-	
+
 	public String getCurrentDateTimeAsString() {
-        // Get the current date and time
-        LocalDateTime currentDateTime = LocalDateTime.now();
+		// Get the current date and time
+		LocalDateTime currentDateTime = LocalDateTime.now();
 
-        // Define the format for the date and time
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+		// Define the format for the date and time
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
-        // Format the current date and time using the formatter
-        String formattedDateTime = currentDateTime.format(formatter);
+		// Format the current date and time using the formatter
+		String formattedDateTime = currentDateTime.format(formatter);
 
-        return formattedDateTime;
-    }
+		return formattedDateTime;
+	}
 
 	// follow another user by their unique username
 	@Transactional
 	public User followUser(String username, String targetUsername) {
 		User currentUser = userRepo.findByUsername(username);
 		User targetUser = userRepo.findByUsername(targetUsername);
-		
+
 		List<String> currentFollowing = currentUser.getFollowing();
-		if(currentFollowing == null) currentFollowing = new ArrayList<>();
-		currentFollowing.add(targetUser.getUsername());
-		
+		if (currentFollowing == null)
+			currentFollowing = new ArrayList<>();
+		if (!currentFollowing.contains(targetUser.getUsername()))
+			currentFollowing.add(targetUser.getUsername());
+
 		currentUser.setFollowing(currentFollowing);
-		
+
 		List<String> targetFollowers = targetUser.getFollowers();
-		if(targetFollowers == null) targetFollowers = new ArrayList<>();
-		targetFollowers.add(currentUser.getUsername());
-		
+		if (targetFollowers == null)
+			targetFollowers = new ArrayList<>();
+		if (!targetFollowers.contains(currentUser.getUsername()))
+			targetFollowers.add(currentUser.getUsername());
+
 		targetUser.setFollowers(targetFollowers);
-		
+
 		userRepo.save(currentUser);
 		return userRepo.save(targetUser);
 	}
-	
+
 	// return the added songs by the user
-	public List<UserSong> addedSongs(String userId){
+	public List<UserSong> addedSongs(String userId) {
 		User user = userRepo.findByiduser(Long.parseLong(userId));
 		if (user == null) {
 			log.info("user YOKKKKK");
 			throw new CustomException("User not found");
 		}
-		
+
 		List<UserSong> userSongs = userSongRepo.findAllByUser(user);
-        return userSongs;
+		return userSongs;
 	}
-	
-	public List<UserAlbum> addedAlbums(String userId){
+
+	public List<UserAlbum> addedAlbums(String userId) {
 		User user = userRepo.findByiduser(Long.parseLong(userId));
 		if (user == null) {
 			log.info("user YOKKKKK");
 			throw new CustomException("User not found");
 		}
-		
+
 		List<UserAlbum> userAlbums = userAlbumRepo.findAllByUser(user);
-        return userAlbums;
+		return userAlbums;
 	}
-	
-	public List<UserArtist> addedArtists(String userId){
+
+	public List<UserArtist> addedArtists(String userId) {
 		User user = userRepo.findByiduser(Long.parseLong(userId));
 		if (user == null) {
 			log.info("user YOKKKKK");
 			throw new CustomException("User not found");
 		}
-		
+
 		List<UserArtist> userArtists = userArtistRepo.findAllByUser(user);
-        return userArtists;
+		return userArtists;
 	}
-	
+
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
- 	public class CustomException extends RuntimeException {
- 		private String message;
+	public class CustomException extends RuntimeException {
+		private String message;
 
-	    public CustomException(String message) {
-	        this.message = message;
-	    }
+		public CustomException(String message) {
+			this.message = message;
+		}
 
-	    public String getMessage() {
-	        return message;
-	    }
+		public String getMessage() {
+			return message;
+		}
 	}
-	
+
 }

a5042d105fd1187ce6feef055ff026ed017a518e
cansnnnn <narsahin161cnsn@gmail.com>
2023-12-11 00:53:20 +0300

    artist recom

---
+++ weezart_web/src/API/RecomReleaseDateApi.jsx
@@ -0,0 +1,42 @@
+async function RecomReleaseDateApi (token, userId ) {
+  
+    const url =`http://localhost:8080/recommendation/genre-artist/${userId}`
+    const auth = "Bearer " + token;
+
+    try{        
+
+        const response = await fetch(url, {
+            headers: {
+                accept: 'application/json',
+                'Authorization': auth,
+                'Content-Type': 'application/json'
+            },
+            method: 'GET',
+            mode: 'cors',
+            credentials: 'include', 
+        });
+        
+        const data = await response.text();
+    
+        if(!response.ok) {
+            throw new Error('Network response is not ok');
+        }
+
+        console.log("data genre artist api : ", data);
+
+        if (data === "") {
+            return "no-song";
+        }
+
+    
+        const newResp = JSON.parse(data); 
+
+        
+        return newResp;
+    }
+    catch (error) {
+        console.error('error in fetching data:', error);
+    }
+}
+
+export default RecomReleaseDateApi;
--- weezart_web/src/Pages/HomePage/Recommendations.jsx
+++ weezart_web/src/Pages/HomePage/Recommendations.jsx
@@ -3,6 +3,7 @@ import RecommendationPopularApi from "../../API/RecommendationPopularApi";
 import RecommendationHotApi from "../../API/RecommendationHotApi";
 import RecommendationFriendApi from "../../API/RecommendationFriend";
 import SongInfoPopup from "../Popups/SongInfoPopup";
+import RecommendationGenreArtistApi from "../../API/RecommendationGenreArtistApi";
 
 const Recommendations = ({ ...props }) => {
   const [recommendationsPopular, setRecommendationsPopular] = useState([]);
@@ -10,6 +11,9 @@ const Recommendations = ({ ...props }) => {
   const [recommendationsFriend, setRecommendationsFriend] = useState("no-song");
   const [recommendationsFriendName, setRecommendationsFriendName] = useState("");
   const [selectedSongInfo, setSelectedSongInfo] = useState(null);
+
+  const [artistRecom, setArtistRecom] = useState("no-song");
+
 
   const fetchRecommendations = async () => {
     const response = await RecommendationPopularApi(props.token);
@@ -19,6 +23,8 @@ const Recommendations = ({ ...props }) => {
     const responseFriend = await RecommendationFriendApi(props.token, props.userId);
     setRecommendationsFriend(responseFriend);
     setRecommendationsFriendName(responseFriend?.friendName);
+    const responseArtist = await RecommendationGenreArtistApi(props.token, props.userId);
+    setArtistRecom(responseArtist);
   };
 
   useEffect(() => {
@@ -50,6 +56,34 @@ const Recommendations = ({ ...props }) => {
     return recommendationsRender;
   };
 
+  const recommendationRenderArtist = (arr) => {
+
+    // followerCount:36625984
+    // genres:['pop', 'r&b']
+    // id:"6vWDO969PvNqNYHIOW5v0m"
+    // imageUrl:"https://i.scdn.co/image/ab6761610000e5eb12e3f20d05a8d6cfde988715"
+    // name:"Beyoncé"
+    
+    let recommendationsRender = [];
+    if (arr !== undefined) {
+      for (let i = 0; i < arr.length; i++) {
+        recommendationsRender.push(
+          <div
+            className="item"
+            key={arr[i].id} // Add a unique key for each recommendation item
+            // onClick={() => openSongInfoPopup(arr[i])}
+          >
+            <img src={arr[i].imageUrl === null ? "https://i.pinimg.com/564x/47/99/fd/4799fdb80098968bf6ff4c311eed1110.jpg" : arr[i].imageUrl} />
+            <div className="play"></div>
+            <h4>{arr[i].name}</h4>
+            <p>Followers: {arr[i].followerCount}</p>
+          </div>
+        );
+      }
+    }
+    return recommendationsRender;
+  };
+
   return (
     <div className="recommendations-body">
       <div className="homepage-recom-content">
@@ -74,6 +108,19 @@ const Recommendations = ({ ...props }) => {
           <p>According to friend {recommendationsFriendName}</p>
           <div className="row">
             <div className="list">{recommendationRender(recommendationsFriend?.friendSongs)}</div>
+          </div>
+          </div>
+        )}
+
+        {artistRecom === "no-song" ? (
+        <div>
+            {/* Render empty div or any alternative content */}
+          </div>
+        ) : (
+          <div>
+          <p>Artists You May Like </p>
+          <div className="row">
+            <div className="list">{recommendationRenderArtist(artistRecom)}</div>
           </div>
           </div>
         )}

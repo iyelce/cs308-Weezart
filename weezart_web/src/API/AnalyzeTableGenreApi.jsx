a09df40c8453f407cf06d9bc28658be9f74b145c
Anil <sinanil2001@gmail.com>
2023-12-28 00:10:07 +0300

    analyse page optimization

---
+++ weezart_web/src/API/AnalyzeTableGenreApi.jsx
@@ -0,0 +1,43 @@
+async function AnalyzeTableGenreApi (token, userId ,filter ) {
+
+    const url=`http://localhost:8080/analysis/${filter}/genre/pop/${userId}`;
+    const auth = "Bearer " + token;
+
+    try{        
+        let cnt=0;
+        const response = await fetch(url, {
+        headers: {
+            accept: 'application/json',
+            'Authorization': auth,
+            'Content-Type': 'application/json'
+        },
+        method: 'GET',
+        mode: 'cors',
+        credentials: 'include'
+    });
+    
+    const data = await response.text();
+
+    if(!response.ok) {
+        throw new Error('Network response is not ok');
+    }
+
+    const newResp = JSON.parse(data); 
+    
+    return newResp;
+
+    }
+    catch (error) {
+        console.error('error in fetching data:', error);
+    }
+
+   
+    
+
+
+
+   
+}
+
+
+export default AnalyzeTableGenreApi;
---
+++ weezart_web/src/API/AnalyzeTableReleaseApi.jsx
@@ -0,0 +1,44 @@
+async function AnalyzeTableReleaseApi (token, userId ,filter ) {
+
+    const result=[];
+    const url=`http://localhost:8080/analysis/${filter}/release-date/2010/2020/${userId}`;
+    const auth = "Bearer " + token;
+
+    try{        
+        let cnt=0;
+        const response = await fetch(url, {
+        headers: {
+            accept: 'application/json',
+            'Authorization': auth,
+            'Content-Type': 'application/json'
+        },
+        method: 'GET',
+        mode: 'cors',
+        credentials: 'include'
+    });
+    
+    const data = await response.text();
+
+    if(!response.ok) {
+        throw new Error('Network response is not ok');
+    }
+
+    const newResp = JSON.parse(data); 
+    
+    return newResp;
+
+    }
+    catch (error) {
+        console.error('error in fetching data:', error);
+    }
+
+   
+    
+
+
+
+   
+}
+
+
+export default AnalyzeTableReleaseApi;

f105d3c98b38c12b0077c3316765c676398c7079
umutkucukkayhan <umut.kucukkayhan@sabanciuniv.edu>
2023-12-28 14:02:52 +0300

    W0-114 <spotify integration added, most listened songs and artists can be returned to the user>

--- cs308-weezart-backend/src/main/java/com/app/controllers/SpotifyController.java
+++ cs308-weezart-backend/src/main/java/com/app/controllers/SpotifyController.java
@@ -1,21 +1,31 @@
 package com.app.controllers;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
-import javax.naming.directory.SearchResult;
-
+import org.apache.commons.codec.binary.Base64;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
-import com.app.filter.JwtTokenFilter;
 import com.app.models.Album;
 import com.app.models.Artist;
 import com.app.models.Song;
@@ -25,56 +35,265 @@ import com.app.services.SpotifyService;
 import com.app.spotify.SpotifyAuthenticator;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
 @RequestMapping("/api/spotify")
 public class SpotifyController {
-	
+
 	private final SpotifyService spotifyService;
-	
-	
-    private static final Logger log = LoggerFactory.getLogger(SpotifyController.class);
-	
+
+	private static final Logger log = LoggerFactory.getLogger(SpotifyController.class);
+
 	@Autowired
 	private final SpotifyAuthenticator spotifyAuthenticator;
-	
-	private final SpotifyAPIService spotifyAPIService = new SpotifyAPIService();
-	
-    @Autowired
-    public SpotifyController(SpotifyService spotifyService, SpotifyAuthenticator spotifyAuthenticator) {
-        this.spotifyService = spotifyService;
-        this.spotifyAuthenticator = spotifyAuthenticator;
-    }
 
-    @GetMapping("/search-artist")
-    public ResponseEntity<List<Artist>> searchArtist(@RequestParam String query) throws JsonMappingException, JsonProcessingException {
-        
-    	log.info("search edicem");
-    	
-    	List<Artist> results = spotifyService.artistSearch(query, spotifyAuthenticator.authenticateWithSpotify());
-        return ResponseEntity.ok(results);
-    }
-    
-    @GetMapping("/search-song")
-    public ResponseEntity<List<Song>> searchSong(@RequestParam String query) throws JsonMappingException, JsonProcessingException {
-    	List<Song> results = spotifyService.songSearch(query, spotifyAuthenticator.authenticateWithSpotify());
-    	
-    	return ResponseEntity.ok(results);
-    }
-    
-    @GetMapping("/search-album")
-    public ResponseEntity<List<Album>> searchAlbum(@RequestParam String query) throws JsonMappingException, JsonProcessingException {
-    	List<Album> results = spotifyService.albumSearch(query, spotifyAuthenticator.authenticateWithSpotify());
-    	
-    	return ResponseEntity.ok(results);
-    }
-    
-    @GetMapping("/get-songs-from-album")
-    public ResponseEntity<Map<String, String>> getSongFromAlbum(@RequestParam String albumId) throws JsonMappingException, JsonProcessingException {
-    	Map<String, String> songIdsAndNames = spotifyAPIService.getSongFromAlbum(albumId, spotifyAuthenticator.authenticateWithSpotify());
-    	
-    	return ResponseEntity.ok(songIdsAndNames);
-    }
-    
+	private final SpotifyAPIService spotifyAPIService = new SpotifyAPIService();
+
+	private final SpotifySearchResponse spotifyParser = new SpotifySearchResponse();
+
+	@Value("${spotify.id}")
+	private String CLIENT_ID;
+
+	@Value("${spotify.secret}")
+	private String CLIENT_SECRET;
+
+	private String REDIRECT_URI = "http://localhost:8080/api/spotify/callback";
+
+	private final RestTemplate restTemplate = new RestTemplate();
+
+	@Autowired
+	public SpotifyController(SpotifyService spotifyService, SpotifyAuthenticator spotifyAuthenticator) {
+		this.spotifyService = spotifyService;
+		this.spotifyAuthenticator = spotifyAuthenticator;
+	}
+
+	@GetMapping("/search-artist")
+	public ResponseEntity<List<Artist>> searchArtist(@RequestParam String query)
+			throws JsonMappingException, JsonProcessingException {
+
+		log.info("search edicem");
+
+		List<Artist> results = spotifyService.artistSearch(query, spotifyAuthenticator.authenticateWithSpotify());
+		return ResponseEntity.ok(results);
+	}
+
+	@GetMapping("/search-song")
+	public ResponseEntity<List<Song>> searchSong(@RequestParam String query)
+			throws JsonMappingException, JsonProcessingException {
+		List<Song> results = spotifyService.songSearch(query, spotifyAuthenticator.authenticateWithSpotify());
+
+		return ResponseEntity.ok(results);
+	}
+
+	@GetMapping("/search-album")
+	public ResponseEntity<List<Album>> searchAlbum(@RequestParam String query)
+			throws JsonMappingException, JsonProcessingException {
+		List<Album> results = spotifyService.albumSearch(query, spotifyAuthenticator.authenticateWithSpotify());
+
+		return ResponseEntity.ok(results);
+	}
+
+	@GetMapping("/get-songs-from-album")
+	public ResponseEntity<Map<String, String>> getSongFromAlbum(@RequestParam String albumId)
+			throws JsonMappingException, JsonProcessingException {
+		Map<String, String> songIdsAndNames = spotifyAPIService.getSongFromAlbum(albumId,
+				spotifyAuthenticator.authenticateWithSpotify());
+
+		return ResponseEntity.ok(songIdsAndNames);
+	}
+
+	@GetMapping("/login")
+	public String loginToSpotify() throws JsonMappingException, JsonProcessingException {
+
+		String scope = "user-top-read";
+
+		String state = UUID.randomUUID().toString().replaceAll("-", "").substring(15);
+
+		String spotifyUrl = "https://accounts.spotify.com/authorize?" + "response_type=code" + "&client_id=" + CLIENT_ID
+				+ "&scope=" + scope + "&redirect_uri=" + REDIRECT_URI + "&state=" + state;
+
+		return spotifyUrl;
+
+	}
+
+	@PostMapping("/callback")
+	public String test(@RequestParam("code") String code, @RequestParam("state") String state)
+			throws JsonMappingException, JsonProcessingException {
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+		MultiValueMap<String, String> formParams = new LinkedMultiValueMap<>();
+		formParams.add("code", code);
+		formParams.add("redirect_uri", REDIRECT_URI);
+		formParams.add("grant_type", "authorization_code");
+
+		String credentials = CLIENT_ID + ":" + CLIENT_SECRET;
+		String base64Credentials = new String(Base64.encodeBase64(credentials.getBytes()));
+
+		headers.set("Authorization", "Basic " + base64Credentials);
+
+		HttpEntity<MultiValueMap<String, String>> requestEntity = new HttpEntity<>(formParams, headers);
+
+		String tokenUrl = "https://accounts.spotify.com/api/token";
+
+		ResponseEntity<String> resp = restTemplate.exchange(tokenUrl, HttpMethod.POST, requestEntity, String.class);
+
+		ObjectMapper objectMapper = new ObjectMapper();
+		JsonNode rootNode = objectMapper.readTree(resp.getBody());
+
+		String accessToken = rootNode.get("access_token").asText();
+
+		return accessToken;
+	}
+
+	@GetMapping("/get-users-top-tracks")
+	public List<Song> getTopTracks(@RequestParam("token") String accessToken)
+			throws JsonMappingException, JsonProcessingException {
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("Authorization", "Bearer " + accessToken);
+
+		UriComponentsBuilder uriBuilder = UriComponentsBuilder // returns 10 songs, range is long term
+				.fromUriString("https://api.spotify.com/v1/me/top/tracks?time_range=long_term&limit=10");
+
+		ResponseEntity<String> result = restTemplate.exchange(uriBuilder.toUriString(), HttpMethod.GET,
+				new HttpEntity<>(headers), String.class);
+
+		List<Song> resultList = new ArrayList<>();
+
+		ObjectMapper objectMapper = new ObjectMapper();
+
+		JsonNode songNode = objectMapper.readTree(result.getBody());
+
+		JsonNode itemsNode = songNode.get("items");
+
+		for (JsonNode item : itemsNode) {
+
+			String id = "", name = "", albumName = "", albumId = "", albumRelease = "", albumImage = "";
+			int popularity = 0, duration_ms = 0;
+			boolean explicit = false;
+
+			List<String> artistsName = new ArrayList<>();
+			List<String> artistsId = new ArrayList<>();
+
+			id = item.get("id").asText();
+
+			name = item.get("name").asText();
+
+			popularity = item.get("popularity").asInt();
+
+			duration_ms = item.get("duration_ms").asInt();
+
+			explicit = item.get("explicit").asBoolean();
+
+			JsonNode albumNode = item.get("album");
+
+			albumId = albumNode.get("id").asText();
+
+			albumName = albumNode.get("name").asText();
+
+			JsonNode imageNode = albumNode.get("images");
+			albumImage = imageNode.get(0).get("url").asText();
+
+			albumRelease = albumNode.get("release_date").asText();
+
+			JsonNode artistNode = item.get("artists");
+
+			for (JsonNode artists : artistNode) {
+				artistsName.add(artists.get("name").asText());
+				artistsId.add(artists.get("id").asText());
+			}
+
+			Song song = new Song(id, albumImage, name, albumName, albumId, artistsName, artistsId, popularity,
+					duration_ms, explicit, albumRelease);
+			resultList.add(song);
+		}
+
+		return resultList;
+	}
+
+	@GetMapping("/get-users-top-artists")
+	public List<Artist> getTopArtists(@RequestParam("token") String accessToken)
+			throws JsonMappingException, JsonProcessingException {
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("Authorization", "Bearer " + accessToken);
+
+		UriComponentsBuilder uriBuilder = UriComponentsBuilder // returns 10 artists, range is long term
+				.fromUriString("https://api.spotify.com/v1/me/top/artists?time_range=long_term&limit=10");
+
+		ResponseEntity<String> result = restTemplate.exchange(uriBuilder.toUriString(), HttpMethod.GET,
+				new HttpEntity<>(headers), String.class);
+
+		List<Artist> resultList = new ArrayList<>();
+
+		ObjectMapper objectMapper = new ObjectMapper();
+
+		JsonNode artistsNode = objectMapper.readTree(result.getBody());
+
+		JsonNode itemsNode = artistsNode.get("items");
+
+		for (JsonNode item : itemsNode) {
+
+			String name = "", imageUrl = "", id = "";
+			List<String> genres = new ArrayList<>();
+			int followerCount = 0;
+
+			id = item.get("id").asText();
+
+			JsonNode followersNode = item.get("followers");
+
+			if (followersNode != null) {
+				followerCount = followersNode.get("total").asInt();
+			}
+
+			JsonNode genresNode = item.get("genres");
+
+			if (genresNode != null) {
+
+				for (JsonNode genreNode : genresNode) {
+					genres.add(genreNode.asText());
+				}
+			}
+
+			name = item.get("name").asText();
+
+			if (item.get("images") != null) {
+				JsonNode imagesNode = item.get("images");
+				if (imagesNode.isArray() && imagesNode.size() > 0) {
+					imageUrl = imagesNode.get(0).get("url").asText();
+				}
+			}
+
+			Artist artist = new Artist(name, genres, imageUrl, followerCount, id);
+
+			resultList.add(artist);
+		}
+		return resultList;
+
+	}
+
+	/*
+	 * NASIL ÇALIŞTIĞINI YAZMASAM UNUTURUM:
+	 * 
+	 * 1. POSTMANDA GİRİŞ YAP
+	 * 
+	 * 2. LOG IN VIA SPOTIFY 1'E ISTEK YOLLA
+	 * 
+	 * 3. DÖNEN LİNKİ TARAYICIYA YAPIŞTIR, SONUCUNDA DÖNDÜRDÜĞÜ LİNKİ POSTMANA AT
+	 * (CALLBACKLI LINK, POST OLACAK, BEARER TOKEN SEÇMEYI UNUTMA)
+	 * 
+	 * 4. ONDAN DÖNEN LİNKİ DE POSTMANDE AÇ, (GET USERS TOP TRACKLİ LİNK, GET
+	 * OLACAK, BEARER TOKEN SEÇMEYİ UNUTMA)
+	 * 
+	 * BUNUN SONUCUNDA TOP TRACKS DÖNÜYOR, BELLİ BİR SAYIYA FİLTRELEYEBİLİRSİN FALAN
+	 * FİLAN
+	 * 
+	 * 
+	 */
+
 }
--- cs308-weezart-backend/src/main/java/com/app/services/SpotifySearchResponse.java
+++ cs308-weezart-backend/src/main/java/com/app/services/SpotifySearchResponse.java
@@ -7,18 +7,12 @@ import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.app.models.Album;
 import com.app.models.Artist;
 import com.app.models.Song;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -31,199 +25,200 @@ public class SpotifySearchResponse {
 //		
 //		
 //	}
-	
-    private static final Logger log = LoggerFactory.getLogger(SpotifyService.class);
-    
-    private final RestTemplate restTemplate = new RestTemplate();
-    
-    private final SpotifyAPIService spotifyAPIService = new SpotifyAPIService();
 
+	private static final Logger log = LoggerFactory.getLogger(SpotifyService.class);
 
-	
+	private final RestTemplate restTemplate = new RestTemplate();
+
+	private final SpotifyAPIService spotifyAPIService = new SpotifyAPIService();
+
 	public List<Artist> artistJsonParser(String artistJson) throws JsonMappingException, JsonProcessingException {
-		
-		
-		
+
 		ObjectMapper objectMapper = new ObjectMapper();
-		
+
 		JsonNode rootNode = objectMapper.readTree(artistJson);
-		
+
 		log.info("root aldım");
-		
+
 		JsonNode artistsNode = rootNode.get("artists");
-		
+
 		JsonNode itemsNode = artistsNode.get("items");
-		
+
 		log.info("items aldım");
-		
+
 		List<Artist> artistList = new ArrayList<>();
-		
+
 		int count = 0;
-		
-		for(JsonNode item : itemsNode) {
-			
+
+		for (JsonNode item : itemsNode) {
+
 			String name = "", imageUrl = "", id = "";
 			List<String> genres = new ArrayList<>();
 			int followerCount = 0;
-			
-			log.info("her itemi aldım " + count);
-			
-			
-			id = item.get("id").asText();
-		
-			
-			JsonNode followersNode = item.get("followers");
-			
-			
-			log.info("follower aldım ");
-			
-			if(followersNode != null)  {followerCount= followersNode.get("total").asInt();}
 
-			
+			log.info("her itemi aldım " + count);
+
+			id = item.get("id").asText();
+
+			JsonNode followersNode = item.get("followers");
+
+			log.info("follower aldım ");
+
+			if (followersNode != null) {
+				followerCount = followersNode.get("total").asInt();
+			}
+
 			JsonNode genresNode = item.get("genres");
-			
+
 			log.info("genres aldım");
-			
-			if(genresNode != null)  {
-				
-				for(JsonNode genreNode : genresNode) {
+
+			if (genresNode != null) {
+
+				for (JsonNode genreNode : genresNode) {
 					genres.add(genreNode.asText());
 				}
 			}
-		
+
 			name = item.get("name").asText();
-			
+
 			log.info(name);
-			
-		if(item.get("images") != null) { 
-			JsonNode imagesNode = item.get("images");
-			if(imagesNode.isArray() && imagesNode.size() > 0) {
-				imageUrl = imagesNode.get(0).get("url").asText(); 
+
+			if (item.get("images") != null) {
+				JsonNode imagesNode = item.get("images");
+				if (imagesNode.isArray() && imagesNode.size() > 0) {
+					imageUrl = imagesNode.get(0).get("url").asText();
+				}
 			}
+
+			Artist artist = new Artist(name, genres, imageUrl, followerCount, id);
+
+			artistList.add(artist);
 		}
-		
-		Artist artist = new Artist(name, genres, imageUrl, followerCount, id);
-		
-		artistList.add(artist);		
-		}		
 		return artistList;
 	}
-	
+
 	public List<Song> songJsonParser(String songJson) throws JsonMappingException, JsonProcessingException {
-			
+
 		List<Song> songList = new ArrayList<>();
-		
+
 		ObjectMapper objectMapper = new ObjectMapper();
-		
+
 		JsonNode rootNode = objectMapper.readTree(songJson);
-		
+
 		JsonNode songNode = rootNode.get("tracks");
-		
+
 		JsonNode itemsNode = songNode.get("items");
-		
-		for(JsonNode item : itemsNode) {	
-			
+
+		for (JsonNode item : itemsNode) {
+
 			String id = "", name = "", albumName = "", albumId = "", albumRelease = "", albumImage = "";
 			int popularity = 0, duration_ms = 0;
 			boolean explicit = false;
-			
+
 			List<String> artistsName = new ArrayList<>();
 			List<String> artistsId = new ArrayList<>();
-			
+
 			id = item.get("id").asText();
-			
+
 			name = item.get("name").asText();
-			
+
 			popularity = item.get("popularity").asInt();
-			
+
 			duration_ms = item.get("duration_ms").asInt();
-			
+
 			explicit = item.get("explicit").asBoolean();
-			
+
 			JsonNode albumNode = item.get("album");
-			
+
 			albumId = albumNode.get("id").asText();
-			
+
 			albumName = albumNode.get("name").asText();
-			
+
 			JsonNode imageNode = albumNode.get("images");
 			albumImage = imageNode.get(0).get("url").asText();
-			
+
 			albumRelease = albumNode.get("release_date").asText();
-			
+
 			JsonNode artistNode = item.get("artists");
-			
-			for(JsonNode artists : artistNode) {
+
+			for (JsonNode artists : artistNode) {
 				artistsName.add(artists.get("name").asText());
 				artistsId.add(artists.get("id").asText());
 			}
-			
-			Song song = new Song(id, albumImage, name, albumName, albumId, artistsName, artistsId, popularity, duration_ms, explicit, albumRelease);
+
+			Song song = new Song(id, albumImage, name, albumName, albumId, artistsName, artistsId, popularity,
+					duration_ms, explicit, albumRelease);
 			songList.add(song);
 		}
-		
-		return songList;	
+
+		return songList;
 	}
-	
-	public List<Album> albumJsonParser(String albumJson, String token) throws JsonMappingException, JsonProcessingException {
-		
-		//TODO songsName and songsId needs a different API call, somehow implement that
-		
+
+	public List<Album> albumJsonParser(String albumJson, String token)
+			throws JsonMappingException, JsonProcessingException {
+
+		// TODO songsName and songsId needs a different API call, somehow implement that
+
 		List<Album> albumList = new ArrayList<>();
-		
+
 		ObjectMapper objectMapper = new ObjectMapper();
 		JsonNode rootNode = objectMapper.readTree(albumJson);
-		
+
 		JsonNode albumNode = rootNode.get("albums");
-		
+
 		JsonNode itemsNode = albumNode.get("items");
-		
-		for(JsonNode item : itemsNode) {
-			
+
+		for (JsonNode item : itemsNode) {
+
 			String id = "", name = "", imageUrl = "", releaseDate = "";
 			int numberOfTracks = 0;
-			
+
 			List<String> artistsName = new ArrayList<>();
 			List<String> artistsId = new ArrayList<>();
-			
+
 			id = item.get("id").asText();
 			name = item.get("name").asText();
 			releaseDate = item.get("release_date").asText();
 			numberOfTracks = item.get("total_tracks").asInt();
-			
+
 			JsonNode imageNode = item.get("images");
 			imageUrl = imageNode.get(0).get("url").asText();
-			
+
 			JsonNode artistsNode = item.get("artists");
-			
-			for(JsonNode artist : artistsNode) {
-				if(artist.get("name") != null) {artistsName.add(artist.get("name").asText()); }
-				if(artist.get("id")!= null) {artistsId.add(artist.get("id").asText()); }
+
+			for (JsonNode artist : artistsNode) {
+				if (artist.get("name") != null) {
+					artistsName.add(artist.get("name").asText());
+				}
+				if (artist.get("id") != null) {
+					artistsId.add(artist.get("id").asText());
+				}
 			}
-			
+
 			List<String> songsId = new ArrayList<>(spotifyAPIService.getSongFromAlbum(id, token).keySet());
 			List<String> songsName = new ArrayList<>(spotifyAPIService.getSongFromAlbum(id, token).values());
-			
-			Album album = new Album(id, name, imageUrl, releaseDate, numberOfTracks, artistsName, artistsId, songsName, songsId);
+
+			Album album = new Album(id, name, imageUrl, releaseDate, numberOfTracks, artistsName, artistsId, songsName,
+					songsId);
 			albumList.add(album);
 		}
 		return albumList;
 	}
-	
 
-	public Map<String, String> songFromAlbumParser(String songFromAlbumJson) throws JsonMappingException, JsonProcessingException {
-		
+	public Map<String, String> songFromAlbumParser(String songFromAlbumJson)
+			throws JsonMappingException, JsonProcessingException {
+
 		Map<String, String> songIdsAndNames = new HashMap<>();
-		
+
 		ObjectMapper objectMapper = new ObjectMapper();
 		JsonNode rootNode = objectMapper.readTree(songFromAlbumJson);
-		
+
 		JsonNode itemsNode = rootNode.get("items");
-		
-		for(JsonNode item : itemsNode) {
+
+		for (JsonNode item : itemsNode) {
 			songIdsAndNames.put(item.get("id").asText(), item.get("name").asText());
 		}
-		
+
 		return songIdsAndNames;
 	}
 
